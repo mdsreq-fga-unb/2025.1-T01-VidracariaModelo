e7a54a3c51b1a85f6a677c984f3cbbd9f674db0b
João Filipe <jfos151104@outlook.com>
2025-07-12 14:10:57 -0300

    banco de dados deploy

--- script/backend/Db.js
+++ script/backend/Db.js
@@ -1,13 +1,11 @@
-// db.js
 require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // necessário para Render
+    }
 });
 
 pool.connect()
